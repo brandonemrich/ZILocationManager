6bf27e6065b123389f67b92cbdae792e996f7f5d
Brandon Emrich <brandon@zueos.com>
2012-06-19 04:13:07 -0400

    First working copy

--- Source/ZILocationManager.h
+++ Source/ZILocationManager.h
@@ -1,18 +1,35 @@
-//
-//  ZILocationManager.h
-//  ZILocationManger
-//
-//  Created by Brandon Emrich on 6/18/12.
-//  Copyright (c) 2012 Zueos, Inc. All rights reserved.
-//
+/*
+ *  ZILocationManager.m
+ *  ZILocationManger
+ *
+ *  Created by Brandon Emrich on 6/18/12.
+ *  Copyright (c) 2012 Zueos, Inc. All rights reserved.
+ *
+ */
 
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
 
+typedef void (^ZILocationManagerCallback)(CLLocation *);
+typedef void (^ZILocationManagerRegionChangeCallback)(CLRegion *);
+typedef void (^ZILocationManagerErrorCallback)(NSError *);
+
 @interface ZILocationManager : NSObject <CLLocationManagerDelegate, MKMapViewDelegate>
-{
-    
-}
+
+@property (nonatomic, copy) ZILocationManagerCallback locationUpdatedBlock;
+@property (nonatomic, copy) ZILocationManagerErrorCallback locationErrorBlock;
+@property (nonatomic, copy) ZILocationManagerRegionChangeCallback regionChangeBlock;
+
+@property (nonatomic, retain) CLLocationManager * locationManager;
+
+@property (nonatomic, retain) CLLocation * currentLocation;
+@property (nonatomic, retain) CLLocation * stubbedLocation;
+
+- (id)initWithFakeLatitude:(CLLocationDegrees)latitude longitude:(CLLocationDegrees)longitude;
+
+- (BOOL) startMonitoringRegionWithCenter:(CLLocationCoordinate2D)coordinate radius:(CLLocationDegrees)radius andIdentifier:(NSString*)identifier;
+
+- (void)findLocation;
 
 @end
--- Source/ZILocationManager.m
+++ Source/ZILocationManager.m
@@ -15,4 +15,101 @@
 
 @implementation ZILocationManager
 
+@synthesize locationManager, currentLocation, locationUpdatedBlock,locationErrorBlock, regionChangeBlock, stubbedLocation;
+
+- (id)init {
+    
+    if ((self = [super init])) {
+        
+        // Setup the location manager 
+        locationManager = [[CLLocationManager alloc] init];
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+        locationManager.distanceFilter = kCLDistanceFilterNone; // or whatever
+        
+    }
+    
+    return self;
+    
+}
+
+- (id)initWithFakeLatitude:(CLLocationDegrees)latitude longitude:(CLLocationDegrees)longitude {
+    
+    if ((self = [self init])) {
+        stubbedLocation = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+    }
+    
+    return self;
+    
+}
+
+
+- (void)findLocation {
+    
+    if (self.stubbedLocation) {
+        [self locationManager:self.locationManager didUpdateToLocation:self.stubbedLocation fromLocation:nil];
+    } else {
+        [self.locationManager startUpdatingLocation];
+    }
+    
+}
+
+- (BOOL) startMonitoringRegionWithCenter:(CLLocationCoordinate2D)coordinate radius:(CLLocationDegrees)radius andIdentifier:(NSString*)identifier {
+    // Do not create regions if support is unavailable or disabled.
+    if ( ![CLLocationManager regionMonitoringAvailable] || ![CLLocationManager regionMonitoringEnabled] )
+        return NO;
+    
+    CLRegion* region = [[CLRegion alloc] initCircularRegionWithCenter:coordinate radius:radius identifier:identifier];
+    [self.locationManager startMonitoringForRegion:region
+                              desiredAccuracy:kCLLocationAccuracyBest];
+    
+    return YES;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    
+    // Test the age of the location measurement to determine if the measurement is cached
+    // in most cases you will not want to rely on cached measurements
+    NSTimeInterval locationAge = -[newLocation.timestamp timeIntervalSinceNow];
+    
+    if (locationAge > 10.0) return;
+    
+    // Test that the horizontal accuracy does not indicate an invalid measurement
+    if (newLocation.horizontalAccuracy < 0) return;
+    
+//    if ([self.currentLocation distanceFromLocation:newLocation] >= 1.0) {
+//        self.regionChangeBlock(newLocation);
+//        self.currentLocation = newLocation;
+//        return;
+//    }
+    
+    // Store the new location
+    self.currentLocation = newLocation;
+    
+    // Call the location updated block
+    if (self.locationUpdatedBlock)
+        self.locationUpdatedBlock(newLocation);
+    
+    // IMPORTANT!!! Minimize power usage by stopping the location manager as soon as possible.
+    //[self.locationManager stopUpdatingLocation];
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    // Call the location error block
+    if (self.locationErrorBlock) {
+        self.locationErrorBlock(error);
+    }
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    self.regionChangeBlock(region);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+   NSLog(@"Exited Region"); 
+}
+
 @end
